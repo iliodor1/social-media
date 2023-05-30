1746f3f919ba0ae44e2b74fdb45092322458b186
Iliodor1 <eldar.gai@yandex.ru>
2023-05-30 19:54:28 +0500

    init project

---
+++ src/test/java/ru/eldar/socialmedia/SocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.eldar.socialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
