1399b35054150b94508db35e3171224d61bf7334
iliodor1 <e9991340203@yandex.ru>
2023-06-01 17:42:44 +0500

    rename User to Account and implement UserDetails interface

--- src/main/java/ru/eldar/socialmedia/service/UserDetailsServiceImpl.java
+++ src/main/java/ru/eldar/socialmedia/service/UserDetailsServiceImpl.java
@@ -5,29 +5,20 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import ru.eldar.socialmedia.entity.User;
-import ru.eldar.socialmedia.repository.UserRepository;
-
-import java.util.Collections;
+import ru.eldar.socialmedia.repository.AccountRepository;
 
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    private final UserRepository userRepository;
+    private final AccountRepository accountRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException(
-                        String.format("User with username %s not found", username))
-                );
-
-        return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
-                user.getPassword(),
-                Collections.emptyList()
-        );
+    public UserDetails loadUserByUsername(String username) {
+        return accountRepository.findByUsername(username)
+                                .orElseThrow(() -> new UsernameNotFoundException(
+                                        String.format("Account %s not found", username))
+                                );
     }
 
 
