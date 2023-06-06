c179e6c69fb993c9907acf12928b8af5b51f901b
Iliodor1 <eldar.gai@yandex.ru>
2023-06-06 10:40:20 +0500

    add java docs and logging

--- src/main/java/ru/eldar/socialmedia/controller/PostController.java
+++ src/main/java/ru/eldar/socialmedia/controller/PostController.java
@@ -1,10 +1,10 @@
 package ru.eldar.socialmedia.controller;
 
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.eldar.socialmedia.dto.PostDto;
 import ru.eldar.socialmedia.mapper.PostMapper;
@@ -13,6 +13,7 @@ import ru.eldar.socialmedia.service.post.PostService;
 import java.security.Principal;
 import java.util.Collection;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/auth/posts")
 @RequiredArgsConstructor
@@ -25,7 +26,10 @@ public class PostController {
     @PostMapping
     @Operation(summary = "Request to create a new post")
     public PostDto create(@RequestBody @Valid PostDto postDto, Principal principal) {
+        log.info("Request to create a new post was received");
+
         var post = postService.create(postMapper.toPost(postDto), principal);
+        log.info("Post created successfully");
 
         return postMapper.toDto(post);
     }
@@ -33,7 +37,9 @@ public class PostController {
     @PutMapping("/{postId}")
     @Operation(summary = "Request to update a post")
     public PostDto update(@RequestBody @Valid PostDto postDto, @PathVariable Long postId, Principal principal) {
+        log.info("Request to update a post was received");
         var post = postService.update(postMapper.toPost(postDto), postId, principal);
+        log.info("Post updated successfully");
 
         return postMapper.toDto(post);
     }
@@ -41,13 +47,17 @@ public class PostController {
     @DeleteMapping("/{postId}")
     @Operation(summary = "Request to delete a post")
     public void delete(@PathVariable Long postId, Principal principal) {
+        log.info("Request to delete a post was received");
         postService.delete(postId, principal);
+        log.info("Post deleted successfully");
     }
 
     @GetMapping("/{userId}")
     @Operation(summary = "Request to get user's posts by user id")
     public Collection<PostDto> getByUserId(@PathVariable Long userId) {
+        log.info("Request to get user's posts by user id was received");
         var posts = postService.getByUserId(userId);
+        log.info("User's posts got successfully");
 
         return postMapper.toDtos(posts);
     }
--- src/main/java/ru/eldar/socialmedia/service/post/PostService.java
+++ src/main/java/ru/eldar/socialmedia/service/post/PostService.java
@@ -5,14 +5,45 @@ import ru.eldar.socialmedia.entity.Post;
 import java.security.Principal;
 import java.util.Collection;
 
+/**
+ * The interface Post service
+ *
+ * @author eldar
+ */
 public interface PostService {
 
+    /**
+     * Creates a new post for the authenticated user.
+     *
+     * @param post      The post to create.
+     * @param principal The principal object representing the authenticated user.
+     * @return The created post.
+     */
     Post create(Post post, Principal principal);
 
+    /**
+     * Updates an existing post for the authenticated user.
+     *
+     * @param post      The updated post data.
+     * @param postId    The ID of the post to update.
+     * @param principal The principal object representing the authenticated user.
+     * @return The updated post.
+     */
     Post update(Post post, Long postId, Principal principal);
 
+    /**
+     * Deletes a post with the given ID for the authenticated user.
+     *
+     * @param postId    The ID of the post to delete.
+     * @param principal The principal object representing the authenticated user.
+     */
     void delete(Long postId, Principal principal);
 
-
+    /**
+     * Retrieves all posts created by the user with the specified ID.
+     *
+     * @param userId The ID of the user whose posts to retrieve.
+     * @return A collection of posts created by the user.
+     */
     Collection<Post> getByUserId(Long userId);
 }
